6e57c5ce8edb7015533c27b06fc72d573b786112
marisstoledo <toledoma@g.crookcounty.orggit config --global marisstoledogit config --global user.email toledoma@g.crookcounty.org>
2021-12-01 08:29:23 -0800

    added header.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './App.css'; 
+import Header from './Header';
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1> Proof of Life!</h1>
+        <Header />
       </div>
     );
   }
