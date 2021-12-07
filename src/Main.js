0fc4dd826a710b4d2f2206dde3d8c64aab238de9
marisstoledo <toledoma@g.crookcounty.orggit config --global marisstoledogit config --global user.email toledoma@g.crookcounty.org>
2021-12-07 08:49:12 -0800

    worked on hornedBeast.js

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,21 @@
+
 import  React from 'react'; 
+import HornedBeast from './HornedBeast';
+import beast from './beastData.json';
+import Container from "react-bootstrap/Container";
 
 class Main extends React.Component {
     render() {
         return (
+            <Container>
             <p>Horned Beasts coming soon..</p>
+            <Row xs={1} md={2} lg={3} xl={4}>
+            {beastData.map((beast, idx) => (
+   		<HornedBeast key={idx} beast={beast} />
+		  ))}
+        </Row>
         )
     }
+    </Container>
 }
 export default Main;
