84e9d252390a6e646b98b33b795b32ae23f46bb0
marisstoledo <toledoma@g.crookcounty.orggit config --global marisstoledogit config --global user.email toledoma@g.crookcounty.org>
2021-12-10 08:47:47 -0800

    added drop down feature

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,14 @@ import HornedBeast from './HornedBeast';
 import beastData from './beastData.json';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import FilterForm from "./FilterForm";
 
 class Main extends React.Component {
     render() {
+        
         return (
             <Container>
+                <FilterForm />
             <Row xs={1} md={2} lg={3} xl={4}>
             {beastData.map((beast, idx) => (
    		<HornedBeast key={idx} beastObj={beast} />
